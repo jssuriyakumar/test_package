c56211efe0c010bc41e08ba6aa50800b326f669f
Suriya Kumar J S <suriyakumar.jatakasaravanan@saipem.com>
2023-02-27 16:16:11 +0530

    modify

--- test_package/test.py
+++ test_package/test.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
-
-def test(self):
-    l = [1,2,3,4]
-    df = pd.DataFrame(l,columns = ['numbers'])
-    return df
+class Test:
+    def __init__(self):
+        pass
+    def test(self):
+        l = [1,2,3,4]
+        df = pd.DataFrame(l,columns = ['numbers'])
+        return df
